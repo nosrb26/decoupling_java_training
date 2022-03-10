ece63106aba743087cc8abd08b8fd53b691cc0e6
Pierre Loizeau <pierre.loizeau@epita.fr>
2022-03-10 12:21:41 +0100

    partie 5

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -3,6 +3,9 @@ package fr.lernejo.logger;
 public class LoggerFactory {
 
     public static Logger getLogger(String name) {
-        return new CompositeLogger(new ContextualLogger(name, new FileLogger("logger.txt")), new ContextualLogger(name, new ConsoleLogger()));
+        return new CompositeLogger(
+            new FilteredLogger(new ContextualLogger(name, new FileLogger("logger.txt")), message -> message.contains("simulation")),
+            new ContextualLogger(name, new ConsoleLogger())
+        );
     }
 }
